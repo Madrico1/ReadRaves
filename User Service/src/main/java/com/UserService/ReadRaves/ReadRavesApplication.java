7f0b2e2bad2e6c46f271a886dd1e5e80a7362f1b
Mehdi MADRANI <mehdimadrani@Mehdis-MacBook-Pro.local>
2024-06-03 00:33:40 +0200

    creating userService Spring project

---
+++ User Service/src/main/java/com/UserService/ReadRaves/ReadRavesApplication.java
@@ -0,0 +1,13 @@
+package com.UserService.ReadRaves;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReadRavesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReadRavesApplication.class, args);
+	}
+
+}
---
+++ User Service/src/test/java/com/UserService/ReadRaves/ReadRavesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.UserService.ReadRaves;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReadRavesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
